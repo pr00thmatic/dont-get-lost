2596388737ab7a39c818e72e7c698d9d39db3290
VG <vengadoravg@gmail.com>
2020-05-23 13:47:05 -0400

    selection and motion

--- Assets/Scripts/Team.cs
+++ Assets/Scripts/Team.cs
@@ -3,15 +3,43 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Team : MonoBehaviour {
+  public static event System.Action<Team> onSelected;
+
   public bool isSelected;
   public Unit head;
   public float speed = 10;
+
+  void OnEnable () {
+    onSelected += Unselect;
+  }
 
   void Update () {
     if (isSelected) {
       head.transform.position += (Vector3.right * Input.GetAxis("Horizontal") +
                                   Vector3.up * Input.GetAxis("Vertical")) *
         speed * Time.deltaTime;
+    }
+  }
+
+  void OnMouseDown () {
+    Select();
+  }
+
+  public void Select () {
+    isSelected = true;
+    foreach (Unit u in GetComponentsInChildren<Unit>()) {
+      u.isSelected = true;
+    }
+
+    if (onSelected != null) onSelected(this);
+  }
+
+  public void Unselect (Team nuSelection) { if (nuSelection != this) Unselect(); }
+
+  public void Unselect () {
+    isSelected = false;
+    foreach (Unit u in GetComponentsInChildren<Unit>()) {
+      u.isSelected = false;
     }
   }
 }
--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -3,4 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Unit : MonoBehaviour {
+  public bool isSelected;
+  public Animator selection;
+
+  void Update () {
+    selection.SetBool("is selected", isSelected);
+  }
 }
